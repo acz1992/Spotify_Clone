4ced964a19fde15c8761b240409d3cbc23d6bfce
acz19 <acz1992@hotmail.co.uk>
2024-01-10 16:00:20 +0100

    Add Sidebar component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Home() {
-	return <div className="text-green-500">Hello Spotify!</div>;
+	return <div className="text-green-500">Main Content</div>;
 }
